aefd6563716bd291bd7e728986e1cdac8518bd2f
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-12-08 14:08:52 +0000

    Assert added

--- play.js
+++ play.js
@@ -2,4 +2,4 @@ const assert = require("assert");
 
 // equal uses == operator, and is now deprecated.
 // strictEqual uses === operator, and is the on eto use...
-assert.strictEqual("2", 2);
+assert.strictEqual("2", 2);     //(actual_result, expected_result) - different to assert in Python.

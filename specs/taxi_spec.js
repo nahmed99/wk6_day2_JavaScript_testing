ad8f6b9d42ed2cd70d1a9ab1807593f12f8a4737
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-12-08 14:41:31 +0000

    added requires and mocha describe function to taxi_spec

---
+++ specs/taxi_spec.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const Taxi = require('../taxi.js'); // Don't need to use the .js in the file name?
+
+// function from mocha suite: describe(name_of_tests, function - inline in the case below)
+describe('Taxi', function () {
+
+
+
+
+})
